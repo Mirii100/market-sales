153fb32c6b924afa006950c0d53edfdbacbfe65d
Mirii100 <njugunaalekii99@gmail.com>
2024-11-26 16:18:30 +0300

    commit rewuirements

--- marketPlace/market/urls.py
+++ marketPlace/market/urls.py
@@ -6,9 +6,9 @@ from django.contrib.auth import views as auth_views
 from .forms import LoginForm
 from  . import views
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('home/',views.home,name='home'),
     path('contact/',views.contact,name='contact'),
-    path('signup/',views.signup,name='signup'),
+    path('',views.signup,name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='login')
 ,
 ]
